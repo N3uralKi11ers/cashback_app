dd302686694ab2fb91bad0ff6d9b0ab46c057166
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-29 23:36:35 +0300

    Remove unnecessary imports

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import IndexScreen from './src/index';
 
 export default function App() {
