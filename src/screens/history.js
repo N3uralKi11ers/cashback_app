e35c89e9e9940d03ed9413224c9a20479aa33640
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-29 23:19:56 +0300

    Add HistoryList

--- src/screens/history.js
+++ src/screens/history.js
@@ -2,27 +2,38 @@ import React from 'react'
 import {View, Text, ScrollView, StyleSheet} from 'react-native'
 import Cell from '../components/history/cell'
 import ExpenseCard from '../components/history/expenseСard';
+import Menu from '../components/menu'
+
+const HistoryList = () => {
+    // use API for this
+
+    const list = [1, 2, 3, 4, 5, 6, 7].map(i => (
+        <Cell key={i} to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
+    ))
+
+    return (
+        <>{list}</>
+    )
+}
+
+const categories = () => {
+    // use API for this
+
+    return [
+        'ТИП ОПЕРАЦИИ',
+        'ПЕРИОД',
+        'КАРТА ИЛИ СЧЕТ',
+        'СУММА',
+        'КЭШБЕК',
+    ]
+}
 
 function HistoryScreen() {
     return (
         <ScrollView style={{backgroundColor: "white", paddingTop: 20}}>
             <ExpenseCard />
-            {/* <Text>Моя история переводов</Text> */}
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
-            <Cell to="YVES ROCHER" type="Оплата товаров и услуг" amount="1234" />
+            <Menu titles={categories()} />
+            <HistoryList />
         </ScrollView>
     );
 }
